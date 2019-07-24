f30b7b8dc9c0bdcd6c75e867dcc81c78d920a21d
gooin <gooin@outlook.com>
2019-07-24 10:40:27 +0800

    clear files

--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="menu-nav">
-    <!--        <img src="../../assets/logo.png" width="24px" height="24px" alt="">-->
-    <!--        <div>-->
-    <!--&lt;!&ndash;            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></g></svg>&ndash;&gt;-->
-    <!--            <img src="../../assets/logo.png" width="24px" height="24px" alt="">-->
-    <!--        </div>-->
+
     <el-tooltip
       v-for="(navItem,index) in nav"
       class="item"
@@ -18,37 +14,6 @@
       </div>
     </el-tooltip>
 
-    <!--   <el-tooltip class="item" effect="dark" :content="nav.explore" placement="right" visible-arrow="false">
-            <div>
-                <img src="../../assets/explore.png" width="24px" height="24px" alt="">
-            </div>
-        </el-tooltip>
-        <el-tooltip class="item" effect="dark" :content="nav.lucky" placement="right" visible-arrow="false">
-            <div>
-                <img src="../../assets/lucky.png" width="24px" height="24px" alt="">
-            </div>
-        </el-tooltip>
-        <el-tooltip class="item" effect="dark" :content="nav.bookmark" placement="right" visible-arrow="false">
-            <div>
-                <img src="../../assets/bookmark.png" width="24px" height="24px" alt="">
-            </div>
-        </el-tooltip>
-        <el-tooltip class="item" effect="dark" :content="nav.share" placement="right" visible-arrow="false">
-            <div>
-                <img src="../../assets/share.png" width="24px" height="24px" alt="">
-            </div>
-        </el-tooltip>
-        <el-tooltip class="item" effect="dark" :content="nav.measure" placement="right" visible-arrow="false">
-            <div>
-                <img src="../../assets/measure.png" width="24px" height="24px" alt="">
-            </div>
-    </el-tooltip>-->
-
-    <!--        <div></div>-->
-    <!--        <div></div>-->
-    <!--        <div></div>-->
-    <!--        <div></div>-->
-    <!--        <div></div>-->
   </div>
 </template>
 
